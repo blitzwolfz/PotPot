96adb5a0746ed8a077a5fb897ffa83ec70a64703
Sam <qureshisamin25@gmail.com>
2020-12-23 14:50:45 -0500

    Muesli is more cringe

--- src/index.ts
+++ src/index.ts
@@ -156,7 +156,9 @@ client.on("message", async message => {
   // }
   let answers:string[] = ["MUESLI IS CRINGE", "muesli is cringe", "Muesli is cringe"]
 
-  if(["meme", "muesli"].includes(message.content.toLowerCase())){
+  if(["meme", "muesli"].includes(message.content.toLowerCase()) 
+  || message.content.toLowerCase().includes("meme") 
+  || message.content.toLowerCase().includes("muesli")){
     message.channel.send(answers[Math.floor(Math.random() * answers.length)])
   }
 
